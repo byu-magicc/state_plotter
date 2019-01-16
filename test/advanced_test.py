b1669a54564dc45315a9d724a6f937aff733d4af
Unknown <skyler237@gmail.com>
2019-01-16 16:11:05 -0700

    Fixed path issue for tests

--- test/advanced_test.py
+++ test/advanced_test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import os, sys
+sys.path.insert(0, os.path.join(os.path.dirname(os.getcwd()), 'src'))
+
 import time
 from builtins import input
 from IPython.core.debugger import set_trace
